5d087512795f9cf394a9fbc91aa74526b9a5bdf5
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-10-15 11:56:35 +0200

    Update Hypr_JaKooLit.sh

--- Hypr_JaKooLit.sh
+++ Hypr_JaKooLit.sh
@@ -3,7 +3,11 @@
 # Clone Hyprland-install.script
 mkdir ~/Data
 cd ~/Data
-git clone --depth=1 https://github.com/JaKooLit/Hyprland-Dots.git
-cd Hyprland-Dots
-chmod +x copy.sh
-./copy.sh
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+cd ~/Arch-Hyprland
+chmod +x install.sh
+./install.sh
+#
+chsh -s $(which zsh)
+zsh
+source ~/.zshrc
