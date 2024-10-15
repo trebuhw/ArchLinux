b4d7aa87e06ec117e7513354edc5a8e6b15710de
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-10-15 09:47:08 +0200

    add JaKoo

---
+++ Hypr_JaKooLit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Clone Hyprland-install.script
+mkdir ~/Data
+cd ~/Data
+git clone --depth=1 https://github.com/JaKooLit/Hyprland-Dots.git
+cd Hyprland-Dots
+chmod +x copy.sh
+./copy.sh
